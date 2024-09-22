67ae0c8ad47ec615837b547d0fec0439724c794d
af009 <afwebd@yahoo.com>
2024-09-22 14:46:40 +0300

    Fixed issue where bottom tab was not removed correctly

--- addons/fuku/fuku.gd
+++ addons/fuku/fuku.gd
@@ -2,21 +2,41 @@
 extends EditorPlugin
 
 var dock: Control
+var added_to_bottom: bool = false #Tracks where the dock was added (Don't touch)
 
-# To move the tab to the bottom panel, change this variable to true and restart the plugin
-const BOTTOM_PANEL: bool = false
+# ----------------USE_BOTTOM_PANEL------------------------
+# Set to true to move the tab to the bottom panel:
+
+# const USE_BOTTOM_PANEL: bool = true
+const USE_BOTTOM_PANEL: bool = false
+
+# Restart plugin to apply changes!
+# --------------------------------------------------------
 
 func _enter_tree() -> void:
-	dock = preload("res://addons/fuku/control.tscn").instantiate()
+	_cleanup()  
 	
-	if BOTTOM_PANEL:
+	dock = preload("res://addons/fuku/control.tscn").instantiate()
+
+	if USE_BOTTOM_PANEL:
 		add_control_to_bottom_panel(dock, "Fuku")
+		added_to_bottom = true
 	else:
 		add_control_to_dock(DOCK_SLOT_RIGHT_UL, dock)
+		added_to_bottom = false
 
 func _exit_tree() -> void:
-	if is_instance_valid(dock):
+	_cleanup()
+
+
+func _cleanup() -> void:
+	if not is_instance_valid(dock):
+		return
+
+	if added_to_bottom:
 		remove_control_from_bottom_panel(dock)
+	else:
 		remove_control_from_docks(dock)
-		dock.queue_free()
+		
+	dock.queue_free()
 	dock = null
